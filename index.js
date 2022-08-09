b3ee436824e2ffeef6a310bbc9c07048c27ebcce
Ben <berkhart@rocketmail.com>
2022-08-08 22:20:35 -0500

    commit tree 2b5bebc6ae6a4f1e3466481935db4172dbe9c0a2

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+
+  if (feet > 2500) {
+    return 'No can do.'
+  };
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } else if (feet < 2000) {
+    return 'That will be twenty bucks.'
+  } else if (feet >= 2000) {
+    return 'I will gladly take your thirty bucks.'
+  };
+  };
+
+scuberGreetingForFeet();
+
+function ternaryCheckCity(city) {
+  if (city === "NYC") {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return 'Thank you so much.'
+      break;
+    case "not as generous":
+      return 'Thank you.'
+      break;
+    case "thanks for everything":
+      return "Bye."
+  }
 }
